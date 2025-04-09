7cc0959364b17431cc3682b659709d2095070272
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-10 00:21:16 +0300

    feat: add initial references

---
+++ main.js
@@ -0,0 +1,4 @@
+const numberInput = document.querySelector("#number");
+const firstOption = document.querySelector("#firstCurrencyOption");
+const secondOption = document.querySelector("#secondCurrencyOption");
+const resultInput = document.querySelector("#result");
