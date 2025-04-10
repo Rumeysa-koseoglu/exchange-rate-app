c2e782ffb366383f04a4dacf890b82311aa49bd3
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-10 15:59:24 +0300

    chore: add comment lines to explain main.js and currency.js logic

--- currency.js
+++ currency.js
@@ -1,11 +1,14 @@
 class Currency {
-    constructor() {
+    constructor() {//Constructor: is the constructive function of this class. the base URL of the API is defined inside.
         this.url = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_vzh0fP41hVFhwQJB9pjH4x4YN7qnylrXoxYj8kyc&base_currency="
     }
 
+    //fetch JSON data from API
     async exchange(value, firstCurrency, secondCurrency) {
+        //add firstCurrency to as base currency in the URL
         const response = await fetch(`${this.url}${firstCurrency}`);
         const result = await response.json();
+        //the amount (value) you enter is multiplied by this rate and the converted value is calculated
         const convertedResult = value * result.data[secondCurrency];
 
         return convertedResult
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const firstOption = document.querySelector("#firstCurrencyOption");
 const secondOption = document.querySelector("#secondCurrencyOption");
 const resultInput = document.querySelector("#result");
 
+//creates an object from Currency class in currency.js file
 const currency = new Currency();
 
 runEventListeners();
@@ -15,15 +16,19 @@ function exchange() {
 
  const inputValue = numberInput.value.trim();
 
+ // if the input area is empty, we also clear result
     if (!inputValue) {
         resultInput.value = "";
         return;
     }
+
+    //get user entered value and currencies
     const value = Number(numberInput.value.trim());
     const firstOptionValue = firstOption.options[firstOption.selectedIndex].textContent;
 
     const secondOptionValue = secondOption.options[secondOption.selectedIndex].textContent;
 
+    // make an API call and calculate the exchange result and display it on the screen
     currency.exchange(value, firstOptionValue, secondOptionValue)
 
     .then((result) => {
